a0a18e71f6581fa9113be11672c92c827d523719
h12345jack <h12345jack@h12345jackdeMacBook-Pro.local>
2018-04-21 14:52:31 +0800

    udpate for data handle

--- handle_datas.py
+++ handle_datas.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import re
 
+from collections import defaultdict
+
+# import matplotlib.pyplot as plt
+import networkx as nx
 
 DATA_DIR = './datas/datas'
 
@@ -58,10 +63,88 @@ def handle_dynasty():
                         print(fname, dynasty)
             except Exception as e:
                 print(fpath, e, file = json_error_f)
-            
+
+def statistic_relation():
+    relations = set()
+    relations_codes = set()
+    relations_c = defaultdict(int)
+    error = 0
+    for fname in os.listdir(DATA_DIR):
+        fpath = os.path.join(DATA_DIR, fname)
+        with open(fpath) as f:
+            try:
+                json_data = json.load(f)
+                if 'Person' not in json_data['Package']['PersonAuthority']['PersonInfo']:
+                    continue
+                person_data = json_data['Package']['PersonAuthority']['PersonInfo']['Person']
+                if 'PersonSocialAssociation' in person_data:
+                    person_association = person_data["PersonSocialAssociation"]
+                    if 'Association' in person_association:
+                        person_assoc = person_association['Association']
+                        if isinstance(person_assoc, list):
+                            for person in person_association['Association']:
+                                if 'AssocName' in person and 'AssocCode' in person:
+                                    relations.add(person['AssocName'])
+                                    relations_codes.add(person['AssocCode'])
+                                    tmp = (person['AssocName'], person['AssocCode'])
+                                    relations_c[tmp] += 1
+                        elif isinstance(person_assoc, dict):
+                            relations.add(person['AssocName'])
+                            relations_codes.add(person['AssocCode'])
+                            tmp = (person['AssocName'], person['AssocCode'])
+                            relations_c[tmp] += 1
+            except json.decoder.JSONDecodeError as e:
+                error += 1
+    print(len(relations), len(relations_codes), len(relations_c))
+    print(relations)
+    print(relations_codes)
+    for r in relations_c:
+        print(r[0], ',', r[1], ',', relations_c[r])
+
+
+def test():
+    entry_dict = defaultdict(int)
+    social_status_dict = defaultdict(int)
+    error = 0
+    for fname in os.listdir(DATA_DIR):
+        fpath = os.path.join(DATA_DIR, fname)
+        with open(fpath) as f:
+            try:
+                json_data = json.load(f)
+                # print(json_data)
+                if 'Person' not in json_data['Package']['PersonAuthority']['PersonInfo']:
+                    continue
+                person_data = json_data['Package']['PersonAuthority']['PersonInfo']['Person']
+                if 'PersonEntryInfo' in person_data and 'Entry' in person_data['PersonEntryInfo']:
+                    tmp = person_data['PersonEntryInfo']['Entry']
+                    if isinstance(tmp, list):
+                        for tmp2 in tmp:
+                            if 'RuShiDoor' in tmp2:
+                                tmp3 = tmp2['RuShiDoor']
+                                entry_dict[tmp3] += 1
+                    else:
+                        tmp3 = tmp['RuShiDoor']
+                        entry_dict[tmp3] += 1
+                if 'PersonSocialStatus' in person_data and 'SocialStatus' in person_data['PersonSocialStatus']:
+                        tmp = person_data['PersonSocialStatus']['SocialStatus']
+                        if isinstance(tmp, list):
+                            for tmp2 in tmp:
+                                tmp3 = tmp2['StatusName']
+                                social_status_dict[tmp3] += 1
+                        else:
+                            tmp3 = tmp['StatusName']
+                            social_status_dict[tmp3] += 1
+            except json.decoder.JSONDecodeError as e:
+                    error += 1
+    print(entry_dict, social_status_dict)
+    
+
 
 def main():
-    handle_dynasty()
+    # handle_dynasty()
+    # test()
+    statistic_relation()
+    
             
 
         
